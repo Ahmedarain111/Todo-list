c80f5336b13a0cb3d93e9fedbcf90ade76acdd8b
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-01-12 13:13:19 +0500

    write function to add task

---
+++ script.js
@@ -0,0 +1,31 @@
+const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" viewBox="0 0 24 24">
+  <line x1="4" y1="4" x2="20" y2="20" />
+  <line x1="20" y1="4" x2="4" y2="20" />
+</svg>
+`
+const taskListDiv = document.querySelector('.task-list');
+function addTask() {
+    const input = document.querySelector("input");
+
+    if (input.value == '') {
+        return;
+    }
+
+    const task = document.createElement("div");
+    task.classList.add('task');
+    const taskName = document.createElement("span");
+    taskName.innerText = input.value;
+    taskName.classList.add("task-name");
+    const removebtn = document.createElement('button');
+    removebtn.innerHTML = svgIcon;
+    removebtn.classList.add('remove');
+    removebtn.addEventListener('click', () => removeTask());
+
+    task.appendChild(taskName);
+    task.appendChild(removebtn);
+    taskListDiv.appendChild(task);
+
+    input.value = '';
+}
+
+document.querySelector(".add-task-btn").addEventListener('click', () => addTask());
