25e4c895405da09833a3c1826d2122b63c9fc78d
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-01-12 13:27:53 +0500

    write way to add task by pressing enter

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
                 <line x1="4" y1="4" x2="20" y2="20" />
                 <line x1="20" y1="4" x2="4" y2="20" />
                 </svg>`;
+
+const input = document.querySelector("input");
 const taskListDiv = document.querySelector('.task-list');
+
+input.addEventListener('keydown', (event) => {
+    if (document.activeElement === input && event.key === "Enter") {
+        addTask();
+    }
+});
 
 document.querySelector(".add-task-btn").addEventListener('click', () => addTask());
 
@@ -11,17 +19,17 @@ function removeTask(task) {
 }
 
 function addTask() {
-    const input = document.querySelector("input");
-
     if (input.value == '') {
         return;
     }
 
     const task = document.createElement("div");
     task.classList.add('task');
+
     const taskName = document.createElement("span");
     taskName.innerText = input.value;
     taskName.classList.add("task-name");
+
     const removebtn = document.createElement('button');
     removebtn.innerHTML = svgIcon;
     removebtn.classList.add('remove');
