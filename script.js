1e3d98b61f2a2d412167d1c297d48b807667dae3
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-01-12 13:19:57 +0500

    write function to remove task

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" viewBox="0 0 24 24">
-  <line x1="4" y1="4" x2="20" y2="20" />
-  <line x1="20" y1="4" x2="4" y2="20" />
-</svg>
-`
+                <line x1="4" y1="4" x2="20" y2="20" />
+                <line x1="20" y1="4" x2="4" y2="20" />
+                </svg>`;
 const taskListDiv = document.querySelector('.task-list');
+
+document.querySelector(".add-task-btn").addEventListener('click', () => addTask());
+
+function removeTask(task) {
+    taskListDiv.removeChild(task);
+}
+
 function addTask() {
     const input = document.querySelector("input");
 
@@ -19,7 +25,7 @@ function addTask() {
     const removebtn = document.createElement('button');
     removebtn.innerHTML = svgIcon;
     removebtn.classList.add('remove');
-    removebtn.addEventListener('click', () => removeTask());
+    removebtn.addEventListener('click', () => removeTask(task));
 
     task.appendChild(taskName);
     task.appendChild(removebtn);
@@ -27,5 +33,3 @@ function addTask() {
 
     input.value = '';
 }
-
-document.querySelector(".add-task-btn").addEventListener('click', () => addTask());
