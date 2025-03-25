ad6fef74667d17d5edcf0dc3a8c5956def243759
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-03-25 17:27:53 +0500

    write "Task" class

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-// const svgIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" viewBox="0 0 24 24">
-//                 <line x1="4" y1="4" x2="20" y2="20" />
-//                 <line x1="20" y1="4" x2="4" y2="20" />
-//                 </svg>`;
+class Task {
+    constructor(title, desc, priority, date, time, status=false) {
+        this.title = title;
+        this.desc = desc;
+        this.priority = priority;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
 
-// const input = document.querySelector("input");
-// const taskListDiv = document.querySelector('.task-list');
+    toggleStatus() {
+        this.status = !this.status;
+    }
+}
 
-// input.addEventListener('keydown', (event) => {
-//     if (document.activeElement === input && event.key === "Enter") {
-//         addTask();
-//     }
-// });
-
-// document.querySelector(".add-task-btn").addEventListener('click', () => addTask());
-
-// function removeTask(task) {
-//     taskListDiv.removeChild(task);
-// }
-
-// function addTask() {
-//     if (input.value == '') {
-//         return;
-//     }
-
-//     const task = document.createElement("div");
-//     task.classList.add('task');
-
-//     const taskName = document.createElement("span");
-//     taskName.innerText = input.value;
-//     taskName.classList.add("task-name");
-
-//     const removebtn = document.createElement('button');
-//     removebtn.innerHTML = svgIcon;
-//     removebtn.classList.add('remove');
-//     removebtn.addEventListener('click', () => removeTask(task));
-
-//     task.appendChild(taskName);
-//     task.appendChild(removebtn);
-//     taskListDiv.appendChild(task);
-
-//     input.value = '';
-// }
