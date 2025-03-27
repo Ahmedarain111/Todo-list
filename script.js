0c87314a2ab9982e2dd82ea3b90d46c4300b9c54
Ahmed <134275679+coder1467@users.noreply.github.com>
2025-03-27 15:13:41 +0500

    Remove time attribute from Task class

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 class Task {
-    constructor(title, desc, priority, date, time, status=false) {
+    constructor(title, desc, priority, date, status=false) {
         this.title = title;
         this.desc = desc;
         this.priority = priority;
         this.date = date;
-        this.time = time;
         this.status = status;
     }
 
